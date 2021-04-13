6a115228f42fcf6d439b27b0c3efc1ddfc95ca98
Paulo Silva <pauloalecioreuel@gmail.com>
2021-04-13 20:35:29 -0300

    Resolvido EXERCÍCIO-1

---
+++ Curso-Java/Aulas/Exercicio1.java
@@ -0,0 +1,20 @@
+/*
+Atividades
+1 - Crie uma nova classe, no método principal imprima um texto no console.
+2 - Compile e execute a classe criada utilizando os comandos Javac e Java.
+Exemplo - No terminal execute:
+javac nome_do_arquivo_java
+em seguida execute:
+        java nome_classe_compilada
+*/
+
+
+class Exercicio1 {
+
+        public static void main(String[] args){
+
+        System.out.println("Olá eu sou Paulo Alécio e estou participando do Imersão DEV da Stefanini \\o/ !!");
+
+        }
+}
+
