ee68c142d22daab064968c9922fd052e14ac4caf
Paulo Silva <pauloalecioreuel@gmail.com>
2021-04-13 22:56:23 -0300

    Resolvido EXERCÍCIO - 2

---
+++ Curso-Java/Aulas/Exercicio2.java
@@ -0,0 +1,39 @@
+/*Crie variáveis para armazenar os seguintes valores e imprima no console:
+
+1 - 2147483649
+2 - 120
+3 - 150
+4 - Bem vindos Devs!
+5 - 0x2202
+6 - 99999.987
+
+*/
+
+class Exercicio2 {
+
+	public static void main (String[] args) {
+
+		long varLong = 2147483649L;
+
+		byte varByte = 120;
+
+		short varShort = 150;
+
+		String varString = "Bem vindo Devs!";
+
+		int varInt = 0x2202;
+
+		double varDouble = 99999.987;
+
+	System.out.println("Iprimindo tipo long " + varLong);
+	System.out.println("Iprimindo tipo byte " + varByte);
+	System.out.println("Iprimindo tipo short " + varShort);
+	System.out.println("Iprimindo tipo String " + varString);
+	System.out.println("Iprimindo tipo int " + varInt);
+	System.out.println("Iprimindo tipo float " + varDouble );	
+	
+	}
+
+
+}
+
